30cabf9e712ee2128dd04939071b2269b84a68d4
VernicaCK <cn5886@srmist.edu.in>
2026-03-16 00:40:38 +0530

    UC5

--- PalindromeCheckApp.java
+++ PalindromeCheckApp.java
@@ -1,40 +1,37 @@
 package PalindromeCheckerApp;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckApp {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the string: ");
         String input = sc.nextLine();
 
-        // Convert string to character array
-        char[] chars = input.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+
+        // Push characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        int start = 0;
-        int end = chars.length - 1;
+        // Pop characters and compare
+        for (int i = 0; i < input.length(); i++) {
+            char ch = stack.pop();
 
-        // Two pointer comparison
-        while (start < end) {
-
-            if (chars[start] != chars[end]) {
+            if (input.charAt(i) != ch) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
-        if (isPalindrome) {
-            System.out.println("It is palindrome? : " + isPalindrome);
-        } else {
-            System.out.println("It is palindrome? : " + isPalindrome);
-        }
+        System.out.println("It is palindrome? : " + isPalindrome);
 
         sc.close();
     }
