a27ad3d0e963e1978ecf7e12be4d554d37a28850
VernicaCK <cn5886@srmist.edu.in>
2026-03-22 14:51:56 +0530

    UC10

--- PalindromeCheckApp.java
+++ PalindromeCheckApp.java
@@ -4,58 +4,6 @@ import java.util.Scanner;
 
 public class PalindromeCheckApp {
 
-    // Node class
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    // Function to check palindrome
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast & slow pointer
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node prev = null;
-        Node curr = slow;
-
-        while (curr != null) {
-            Node next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        // Compare first half and reversed second half
-        Node first = head;
-        Node second = prev;
-
-        while (second != null) {
-            if (first.data != second.data) {
-                return false;
-            }
-            first = first.next;
-            second = second.next;
-        }
-
-        return true;
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -63,25 +11,26 @@ public class PalindromeCheckApp {
         System.out.print("Enter the string: ");
         String input = sc.nextLine();
 
-        // Convert string to linked list
-        Node head = null;
-        Node temp = null;
+        // Normalize string: remove spaces & convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
-        for (int i = 0; i < input.length(); i++) {
-            Node newNode = new Node(input.charAt(i));
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
 
-            if (head == null) {
-                head = newNode;
-                temp = newNode;
-            } else {
-                temp.next = newNode;
-                temp = newNode;
+        // Check palindrome
+        while (start < end) {
+
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
             }
+
+            start++;
+            end--;
         }
 
-        boolean result = isPalindrome(head);
-
-        System.out.println("It is palindrome? : " + result);
+        System.out.println("It is palindrome? : " + isPalindrome);
 
         sc.close();
     }
